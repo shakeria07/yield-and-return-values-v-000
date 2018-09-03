81d2921ad362374c78068a0c8d506f2ac73d9887
Shakeria Smith <shakerias2@gmail.com>
2018-09-03 20:24:08 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,15 @@ def hello(array)
   end
 end
 
-
+binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
